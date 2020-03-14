cd7f16074827115ef147f944858483ba96d837eb
Luis Ramon Saenz de Buruaga <celoga221122@gmail.com>
2020-03-14 17:03:41 +0100

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,15 @@
 // puerto
 process.env.PORT = process.env.PORT || 3000;
+
+// entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// base datost
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://Alba:54podeqmT8jC1lFR@cluster0-mq0ph.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
